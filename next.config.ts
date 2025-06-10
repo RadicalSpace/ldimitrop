510fa63797018c35ea23d61c97c9f8294252fb82
RadicalSpace <leandrosdim@gmail.com>
2025-06-10 12:25:42 -0700

    ssg export

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,12 @@ import type { NextConfig } from "next";
 import createMDX from '@next/mdx'
  
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   transpilePackages: ['next-mdx-remote'],
   // Optionally, add any other Next.js config below
+  output: 'export',
 }
  
 const withMDX = createMDX({
