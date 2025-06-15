198589359c008dc74b13a6948bcaad04bfecc85a
RadicalSpace <leandrosdim@gmail.com>
2025-06-14 17:56:57 -0700

    k

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,14 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === 'production'
 
 /** @type {import('next').NextConfig} */
-const nextConfig: NextConfig = {
+const nextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   transpilePackages: ['next-mdx-remote'],
   // Optionally, add any other Next.js config below
   output: 'export',
   basePath: '/ldimitrop',
-  assetPrefix: '/ldimitrop',
-  trailingSlash: true
+  assetPrefix: '/ldimitrop/',
 }
 
 export default nextConfig
